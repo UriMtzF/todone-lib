280c3926963a220506eb0437aa731ae73edbc93a
UriMtzF <39018208+UriMtzF@users.noreply.github.com>
2024-02-02 13:59:49 -0600

    Fixed tests to comply with new standard

--- test/todone_lib_test.dart
+++ test/todone_lib_test.dart
@@ -4,11 +4,11 @@ import 'package:todone_lib/todone_lib.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final Parser parser = Parser();
+  final ToDone parser = ToDone();
 
   group('All ways of input working:', () {
     test('Works with String', () {
-      List<Task> tasks = parser.parseTasksFromString('- Valid task');
+      List<Task> tasks = parser.parseTasksFromString('! Valid task');
       expect(tasks.isEmpty, false);
     });
     test('Works with file path', () {
@@ -24,12 +24,12 @@ void main() {
   group('Is valid task:', () {
     group('Only works with valid syntax:', () {
       test('Works with one task', () {
-        String text = '- This is a valid task';
+        String text = '! This is a valid task';
         List<Task> tasks = parser.parseTasksFromString(text);
         expect(tasks[0].title, 'This is a valid task');
       });
       test('Works with multiples tasks', () {
-        String text = '- First task\n- Second task';
+        String text = '! First task\n! Second task';
         List<Task> tasks = parser.parseTasksFromString(text);
         expect(tasks[0].title, 'First task');
         expect(tasks[1].title, 'Second task');
@@ -52,28 +52,28 @@ void main() {
 
   group('Can parse priority:', () {
     test('Right syntax', () {
-      String text = '- (A) This has an A priority';
+      String text = '! (A) This has an A priority';
       List<Task> tasks = parser.parseTasksFromString(text);
       expect(tasks[0].priority, "A");
     });
     test('Only takes first coincidence', () {
-      String text = '- (A) (B) This has an A priority';
+      String text = '! (A) (B) This has an A priority';
       List<Task> tasks = parser.parseTasksFromString(text);
       expect(tasks[0].priority, "A");
     });
     test('Does not take lowercase', () {
-      String text = '- (a) This is not a priority';
+      String text = '! (a) This is not a priority';
       List<Task> tasks = parser.parseTasksFromString(text);
       expect(tasks[0].priority, "");
     });
     test('Wrong syntax', () {
-      String text = '- (A)This is not valid';
+      String text = '! (A)This is not valid';
       List<Task> tasks = parser.parseTasksFromString(text);
       expect(tasks[0].priority, "");
     });
     test('Does not take other words in parentheses', () {
       String text =
-          '- This has (a little secret)\n- (A) This also has (a secret)';
+          '! This has (a little secret)\n! (A) This also has (a secret)';
       List<Task> tasks = parser.parseTasksFromString(text);
       expect(tasks[0].priority, "");
       expect(tasks[1].priority, "A");
