80930fb5047ed906abf470059328c8b605720564
UriMtzF <39018208+UriMtzF@users.noreply.github.com>
2024-02-02 13:59:32 -0600

    Created method to write to file
    Added documentation to all public APIs

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -1,16 +1,62 @@
 import 'dart:io';
 import 'task.dart';
 
-class Parser {
+/// A parser that can write and read [Task] objects.
+class ToDone {
+  /// Either opens or creates a file in the provided path to write all the tasks in the [List] provided, the content of the file will be overwritten.
+  void writeToFilePath(String path, List<Task> tasks) {
+    writeToFile(File(path), tasks);
+  }
+
+  /// Either opens or creates a file in the provided [File] to write all the tasks in the [List] provided, the content of the file will be overwritten.
+  void writeToFile(File file, List<Task> tasks) {
+    file.writeAsStringSync(_stringFromTasks(tasks), mode: FileMode.writeOnly);
+  }
+
+  /// From a [List] of [Task]s will generate a correctly formated [String] compatible with the ToDone format.
+  String _stringFromTasks(List<Task> tasks) {
+    String fileContent = '';
+    for (Task task in tasks) {
+      String line = '! ';
+      if (task.status == Status.done) {
+        if (task.doneDate == null) {
+          line += 'done: ';
+        } else {
+          line += 'done:${task.doneDate.toString().substring(0, 10)} ';
+        }
+      }
+      if (task.priority.isNotEmpty) {
+        line += '(${task.priority}) ';
+      }
+      if (task.dueDate != null) {
+        line += 'due:${task.dueDate.toString().substring(0, 10)} ';
+      }
+      line += '${task.title} ';
+      if (task.creationDate != null) {
+        line += 'created:${task.creationDate.toString().substring(0, 10)} ';
+      }
+      fileContent += '$line ';
+    }
+    return fileContent;
+  }
+
+  /// Returns a [List] of [Task]s from a [String] of text.
+  /// If no task is found it will return an empty [List].
   List<Task> parseTasksFromString(String text) {
     List<String> lines = text.split("\n");
     return _parseTasks(lines);
   }
 
+  /// Returns a [List] of [Task]s read from a file.
+  /// The absolute or relative path of the must be provided
+  /// If no task is found it will return an empty [List].
   List<Task> parseTasksFromPath(String filePath) {
     return parseTasksFromFile(File(filePath));
   }
 
+  /// Returns a [List] of [Task]s read from a [File].
+  /// A [File] object must be provided
+  /// If no task is found it will return an empty [List].
   List<Task> parseTasksFromFile(File file) {
     if (!file.existsSync()) {
       throw Exception('Files does not exists');
@@ -103,7 +149,7 @@ class Parser {
           "done: ${task.doneDate.toString().substring(0, 10)}", "");
     }
     if (task.priority.isNotEmpty) {
-      taskText = taskText.replaceAll(RegExp(" \\([A-Z]\\) "), "");
+      taskText = taskText.replaceAll(RegExp("\\([A-Z]\\)"), "");
     }
     if (task.creationDate != null) {
       taskText = taskText.replaceAll(
--- lib/src/task.dart
+++ lib/src/task.dart
@@ -1,8 +1,10 @@
+/// Used in the [status] field of a [Task] object, is either done or undone.
 enum Status {
   done,
   undone,
 }
 
+/// An object that has all the fields supported by the ToDone standard.
 class Task {
   Status status;
   DateTime? doneDate;
